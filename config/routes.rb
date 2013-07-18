36decde2240deee3e73a9e0f1d2522fe4f015cc4
sunnykhatter <lakshaykhatter@gmail.com>
2013-07-18 15:30:05 -0400

    Writing tests for browsing projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
-Crowdfunder::Application.routes.draw do
+  Crowdfunder::Application.routes.draw do
+
+    resources :projects
+
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +60,4 @@ Crowdfunder::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+# end
--- test/integration/project_flows_test.rb
+++ test/integration/project_flows_test.rb
@@ -1,29 +1,25 @@
 require 'test_helper'
 
 class ProjectFlowsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "browsing projects" do 
+    # Create three projects
+    project1 = FactoryGirl.create(:project, :title => "Project 1")
+    project2 = FactoryGirl.create(:project, :title => "Project 2")
+    project3 = FactoryGirl.create(:project, :title => "Project 3")
 
-  test "browsing projects" do
-  	project1 = FactoryGirl.create(:project, :title => "Project 1")
-  	project2 = FactoryGirl.create(:project, :title => "project 2")
-  	project3 = FactoryGirl.create(:project, :title => "project 3")
+    # Go to the page with the projects
+    visit "/projects"
+    # Assert the page we're on is the page with the projects
+    assert_equal projects_path, current_path
 
-  #Go to the page with projects
-  visit "/projects"
+    # Assert this page has the words 'Listing Projects'
+    assert page.has_content?('Listing projects')
 
-  #Go to the page with the projects
-  assert_equal projects_path, current_path
+    # Assert this page has these words as well
+    assert page.has_content?('Project 1')
+    assert page.has_content?('Project 2')
+    assert page.has_content?('Project 3')
 
-  #Assert this page has the words 'Listing Projects'
-  assert page.has_content?('Listing Projects')
-
-  #Assert this page has these words as well
-  assert page.has_content?('Project 1')
-  assert page.has_content?('Project 2')
-  assert page.has_content?('Project 3')
-end
-
-
+  end
 end
